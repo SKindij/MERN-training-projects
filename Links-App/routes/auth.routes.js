dd77b987751806fb4cc9e09926cb33ceb12d9ae6
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-10-10 22:12:35 +0300

    init: create project structure and initial files
    create auth.routes.js

---
+++ Links-App/routes/auth.routes.js
@@ -0,0 +1,18 @@
+// auth.routes.js
+const {Router} = require('express');
+
+const router = Router();
+
+// =>> /api/auth/register
+
+
+
+
+
+
+// =>> /api/auth/login
+
+
+
+
+module.exports = router
