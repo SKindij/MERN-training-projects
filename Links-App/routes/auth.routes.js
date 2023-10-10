2db5e9f3cb02e72023a31ef0f5a7be4c3d36d753
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-10-10 22:52:12 +0300

    feat: introduce user authentication feature
    update auth.routes.js

--- Links-App/routes/auth.routes.js
+++ Links-App/routes/auth.routes.js
@@ -47,8 +47,48 @@ router.post(
 })
 
 // =>> /api/auth/login
-
-
-
+router.post(
+    '/login',
+    [
+      check('email', 'enter correct email').normalizeEmail().isEmail(),
+      check('password', 'enter password').exists()
+    ],
+    async (req, res) => {
+    try {
+      const errors = validationResult(req)
+  
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: 'Incorrect login information'
+        })
+      }
+  
+      const {email, password} = req.body
+  
+      const user = await User.findOne({ email })
+  
+      if (!user) {
+        return res.status(400).json({ message: 'User is not found' })
+      }
+  
+      const isMatch = await bcrypt.compare(password, user.password)
+  
+      if (!isMatch) {
+        return res.status(400).json({ message: 'Invalid password, try again' })
+      }
+  
+      const token = jwt.sign(
+        { userId: user.id },
+        config.get('jwtSecret'),
+        { expiresIn: '1h' }
+      )
+  
+      res.json({ token, userId: user.id })
+  
+    } catch (e) {
+      res.status(500).json({ message: 'Something went wrong, try again' })
+    }
+});
 
 module.exports = router
