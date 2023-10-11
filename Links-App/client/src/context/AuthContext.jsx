d2a84d75285ce8541779bf6ec39b85425d6e0548
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-10-12 00:03:52 +0300

    init: set up project structure and dependencies
    create AuthContext.jsx

---
+++ Links-App/client/src/context/AuthContext.jsx
@@ -0,0 +1,12 @@
+// AuthContext.jsx
+import {createContext} from 'react'
+
+function noop() {}
+
+export const AuthContext = createContext({
+  token: null,
+  userId: null,
+  login: noop,
+  logout: noop,
+  isAuthenticated: false
+})
