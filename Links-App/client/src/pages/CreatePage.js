b43ac954d6d39d85fdd6d9762a530c0f77e1df64
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-10-11 23:32:28 +0300

    feat: implement new module
    update CreatePage.js

--- Links-App/client/src/pages/CreatePage.js
+++ Links-App/client/src/pages/CreatePage.js
@@ -1,9 +1,36 @@
 // CreatePage.js
+import {useContext, useEffect, useState} from 'react'
+
+
 export const CreatePage = () => {
 
-    return (
-          <div>
-            <h1>Create Page</h1>
-          </div>
-    )
+
+  const [link, setLink] = useState('')
+
+  const pressHandler = async event => {
+    if (event.key === 'Enter') {
+      try {
+        console.log("after press Enter:", link)
+      } catch (e) {console.log(e)}
+    }
+  }
+
+  return (
+    <>
+      <h2>Create Link Modele</h2>
+      <div className="row">
+      <div className="col s8 offset-s2" style={{paddingTop: '2rem'}}>
+        <div className="input-field">
+          <input
+            placeholder="Paste link"
+            id="link" type="text" value={link}
+            onChange={e => setLink(e.target.value)}
+            onKeyPress={pressHandler}
+          />
+          <label htmlFor="link">Enter link</label>
+        </div>
+      </div>
+      </div>
+    </>
+  )
 }
