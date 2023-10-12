b2951198f10b948fb53576a1313624422ac438d9
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-10-12 22:11:26 +0300

    docs: clarify descriptions of key functionalities
    update http.hook.jsx

--- Links-App/client/src/hooks/http.hook.jsx
+++ Links-App/client/src/hooks/http.hook.jsx
@@ -5,28 +5,32 @@ export const useHttp = () => {
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
 
+  // function allows making asynchronous HTTP requests
   const request = useCallback(async (url, method = 'GET', body = null, headers = {}) => {
+    // to indicate request is in progress
     setLoading(true)
     try {
       if (body) {
         body = JSON.stringify(body)
         headers['Content-Type'] = 'application/json'
       }
-
+      // send the request
       const response = await fetch(url, {method, body, headers})
+      // parse the response data
       const data = await response.json()
 
       if (!response.ok) {
-        throw new Error(data.message || 'Что-то пошло не так')
+        throw new Error(data.message || 'Something went wrong')
       }
-
+      // once the request is completed
       setLoading(false)
-
+      // return the parsed data
       return data
     } catch (e) {
-      setLoading(false)
-      setError(e.message)
-      throw e
+      setLoading(false);
+      setError(e.message);
+      // rethrow error for handling in higher-level components
+      throw e;
     }
   }, [])
 
