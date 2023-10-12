a08a05f2432e10c3528b9f7d0e576cdfcecd14cc
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-10-12 21:42:35 +0300

    docs: clarify descriptions of key functionalities
    update auth.hook.jsx

--- Links-App/client/src/hooks/auth.hook.jsx
+++ Links-App/client/src/hooks/auth.hook.jsx
@@ -1,36 +1,38 @@
 // auth.hook.jsx
 import {useState, useCallback, useEffect} from 'react'
 
-const storageName = 'userData'
+const storageName = 'userData';
 
 export const useAuth = () => {
-  const [token, setToken] = useState(null)
-  const [ready, setReady] = useState(false)
-  const [userId, setUserId] = useState(null)
+  const [token, setToken] = useState(null);
+  const [ready, setReady] = useState(false);
+  const [userId, setUserId] = useState(null);
 
   const login = useCallback((jwtToken, id) => {
-    setToken(jwtToken)
-    setUserId(id)
-
+    setToken(jwtToken);
+    setUserId(id);
+    // store user data in local storage as JSON string
     localStorage.setItem(storageName, JSON.stringify({
       userId: id, token: jwtToken
     }))
   }, [])
 
-
   const logout = useCallback(() => {
-    setToken(null)
-    setUserId(null)
+    setToken(null);
+    setUserId(null);
     localStorage.removeItem(storageName)
   }, [])
 
+  // to load user data from local storage when component is mounted
   useEffect(() => {
     const data = JSON.parse(localStorage.getItem(storageName))
 
     if (data && data.token) {
+      // automatically log in user if token is found
       login(data.token, data.userId)
     }
-    setReady(true)
+    // to indicate that authentication data has been loaded
+    setReady(true);
   }, [login])
 
 
