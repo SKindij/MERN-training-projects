7f32ae1f0f6860f4c7d029e8cec05e57362a3233
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-10-12 23:06:13 +0300

    style: format codebase for consistent style
    update CreatePage.js

--- Links-App/client/src/pages/CreatePage.js
+++ Links-App/client/src/pages/CreatePage.js
@@ -1,16 +1,29 @@
 // CreatePage.js
 import {useContext, useEffect, useState} from 'react'
+import {useHttp} from '../hooks/http.hook'
+import {AuthContext} from '../context/AuthContext'
+import {useHistory} from 'react-router-dom'
 
-
+// This component represents the page for creating new links.
 export const CreatePage = () => {
-
-
+  const history = useHistory()
+  const auth = useContext(AuthContext)
+  const {request} = useHttp()
   const [link, setLink] = useState('')
+
+  useEffect(() => {
+    window.M.updateTextFields()
+  }, [])
 
   const pressHandler = async event => {
     if (event.key === 'Enter') {
       try {
-        console.log("after press Enter:", link)
+        console.log("press Enter");
+        const data = await request('/api/link/generate', 'POST', {from: link}, {
+          Authorization: `Bearer ${auth.token}`
+        })
+        // redirect to detail page for newly created link
+        history.push(`/detail/${data.link._id}`)
       } catch (e) {console.log(e)}
     }
   }
@@ -21,8 +34,7 @@ export const CreatePage = () => {
       <div className="row">
       <div className="col s8 offset-s2" style={{paddingTop: '2rem'}}>
         <div className="input-field">
-          <input
-            placeholder="Paste link"
+          <input placeholder="Paste link"
             id="link" type="text" value={link}
             onChange={e => setLink(e.target.value)}
             onKeyPress={pressHandler}
