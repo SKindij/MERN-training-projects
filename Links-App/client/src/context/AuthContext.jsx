f77b48b01b37342f1511028dbec1fccc25f24f28
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-10-12 20:55:55 +0300

    docs: clarify descriptions of key functionalities
    update AuthContext.jsx

--- Links-App/client/src/context/AuthContext.jsx
+++ Links-App/client/src/context/AuthContext.jsx
@@ -1,12 +1,19 @@
 // AuthContext.jsx
 import {createContext} from 'react'
 
+/*
+  define placeholder function "noop" (no operation)
+  This function does nothing and serves as default value 
+  for the login and logout properties in the context.
+*/
 function noop() {}
 
+// create an authentication context
+// it is meant to manage authentication-related data and functions
 export const AuthContext = createContext({
-  token: null,
+  token: null, // token for user authentication
   userId: null,
   login: noop,
   logout: noop,
-  isAuthenticated: false
+  isAuthenticated: false // flag indicating whether user is authenticated
 })
