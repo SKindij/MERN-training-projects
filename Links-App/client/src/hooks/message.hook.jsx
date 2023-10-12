80f614d388d63f1aca98b3ac27e7de6dcb920277
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-10-12 22:17:10 +0300

    style: enhance code comments
    update message.hook.jsx

--- Links-App/client/src/hooks/message.hook.jsx
+++ Links-App/client/src/hooks/message.hook.jsx
@@ -1,10 +1,14 @@
 // message.hook.jsx
 import {useCallback} from 'react'
 
+// define hook, which allows displaying toast messages
 export const useMessage = () => {
   return useCallback(text => {
+    // check if Materialize CSS library is available and if 'text' is provided.
     if (window.M && text) {
+      // display toast message using Materialize CSS
       window.M.toast({ html: text })
     }
   }, [])
 }
+// to show user-friendly notifications or messages in application
