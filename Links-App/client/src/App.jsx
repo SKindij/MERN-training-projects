b8774edd8774e07e1d993129bcdd5ed2e9dd4464
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-10-13 21:30:44 +0300

    init: set up project structure and dependencies
    update App.jsx

--- Links-App/client/src/App.jsx
+++ Links-App/client/src/App.jsx
@@ -1,16 +1,40 @@
-
-
-
+// App.jsx
+import {BrowserRouter as Router} from 'react-router-dom'
+import {useRoutes} from './routes'
+import {useAuth} from './hooks/auth.hook'
+import {AuthContext} from './context/AuthContext'
+import {Navbar} from './components/Navbar'
+import {Loader} from './components/Loader'
+import 'materialize-css';
 import './App.css'
 
 function App() {
+  // to manage user authentication
+  const {token, login, logout, userId, ready} = useAuth();
+  // check if user is authenticated based on presence of token
+  const isAuthenticated = !!token;
+  // define the application's routing
+  const routes = useRoutes(isAuthenticated);
 
+  if (!ready) {
+    // display spinner while app is preparing
+    return <Loader />
+  }
 
   return (
-    <>
-      <h1>MERN Links App</h1>
-
-    </>
+    <AuthContext.Provider value={{
+      /* wraps app, providing authentication-related data to child components */
+      token, login, logout, userId, isAuthenticated
+    }}>
+      <p>MERN Links App</p>
+      <Router>
+        { isAuthenticated && <Navbar /> /* display navigation if user is authenticated */}
+        <div className="container">
+          {/* display app's routes */}
+          {routes} 
+        </div>
+      </Router>
+    </AuthContext.Provider>
   )
 }
 
