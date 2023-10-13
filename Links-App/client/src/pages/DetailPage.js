38c0ae350bce93bf768cef2cf5cf9129d04a8e96
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-10-13 20:36:08 +0300

    docs: write description of certain functionality
    update DetailPage.js

--- Links-App/client/src/pages/DetailPage.js
+++ Links-App/client/src/pages/DetailPage.js
@@ -1,9 +1,44 @@
 // DetailPage.js
-export const DetailPage = () => {
+// component represents the detail page for a specific link
+import {useCallback, useContext, useEffect, useState} from 'react'
+import {useParams} from 'react-router-dom'
+// to manage HTTP requests
+import {useHttp} from '../hooks/http.hook'
+// allows access to authentication context, providing authentication token
+import {AuthContext} from '../context/AuthContext'
+import {Loader} from '../components/Loader'
+import {LinkCard} from '../components/LinkCard'
 
-    return (
-          <div>
-            <h1>Detail Page</h1>
-          </div>
-    )
+export const DetailPage = () => {
+  const {token} = useContext(AuthContext)
+  const {request, loading} = useHttp()
+  const [link, setLink] = useState(null)
+  const linkId = useParams().id
+
+  // function to retrieve link details
+  const getLink = useCallback(async () => {
+    try {
+      const fetched = await request(`/api/link/${linkId}`, 'GET', null, {
+        Authorization: `Bearer ${token}`
+      })
+      setLink(fetched)
+    } catch (e) {console.log(e)}
+  }, [token, linkId, request])
+
+  useEffect(() => {
+    // fetch link details when component is mounted
+    getLink()
+  }, [getLink])
+
+  if (loading) {
+    // display spinner while fetching data
+    return <Loader />
   }
+
+  return (
+    <>
+    {/* display link card when loading has finished and link has been fetched */}
+      { !loading && link && <LinkCard link={link} /> }
+    </>
+  )
+}
