6a4c291211143b7cdd67d8111d21d975e61cbeeb
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-10-13 20:45:44 +0300

    feat: create new data visualization componen
    update LinksPage.js

--- Links-App/client/src/pages/LinksPage.js
+++ Links-App/client/src/pages/LinksPage.js
@@ -1,9 +1,42 @@
 // LinksPage.js
-export const LinksPage = () => {
+// component represents user's links page, displaying list of their shortened links
+import {useCallback, useContext, useEffect, useState} from 'react';
+// is used to manage HTTP requests
+import {useHttp} from '../hooks/http.hook';
+// allows access to authentication context, providing authentication token
+import {AuthContext} from '../context/AuthContext';
+import {Loader} from '../components/Loader';
+import {LinksList} from '../components/LinksList';
 
-    return (
-          <div>
-            <h1>Links Page</h1>
-          </div>
-    )
+export const LinksPage = () => {
+  const [links, setLinks] = useState([])
+  const {loading, request} = useHttp()
+  const {token} = useContext(AuthContext)
+
+  // function to fetch and set user's links
+  const fetchLinks = useCallback(async () => {
+    try {
+      const fetched = await request('/api/link', 'GET', null, {
+        Authorization: `Bearer ${token}`
+      })
+      setLinks(fetched)
+    } catch (e) {console.log(e)}
+  }, [token, request])
+
+  useEffect(() => {
+    // fetch user's links when omponent is mounted
+    fetchLinks()
+  }, [fetchLinks])
+
+  if (loading) {
+    // display spinner while fetching data
+    return <Loader/>
+  }
+
+  return (
+    <>
+    {/* display component when loading has finished and links have been fetched */}
+      {!loading && <LinksList links={links} />}
+    </>
+  )
 }
