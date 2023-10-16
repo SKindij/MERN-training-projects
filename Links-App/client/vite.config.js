2a3a17d8a21d94953a482e3c6807694b5521e4c9
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-10-16 21:10:54 +0300

    fix: handle API response errors
    update vite.config.js

--- Links-App/client/vite.config.js
+++ Links-App/client/vite.config.js
@@ -4,14 +4,27 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api':'http://localhost:5000',
+    },
+  },
   plugins: [react()],
+});
+
+
+/*
+export default defineConfig({  
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
+        secure: false,
         rewrite: (path) => path.replace(/^\/api/, '')
       },
     },
   },
+  plugins: [react()],
 })
+*/
