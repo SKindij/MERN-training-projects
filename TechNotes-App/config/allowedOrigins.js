80eaa48119f7a42fb3452a00f904b046c557eda6
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-10-22 20:43:24 +0300

    init: create an additional file
    create allowedOrigins.js

---
+++ TechNotes-App/config/allowedOrigins.js
@@ -0,0 +1,8 @@
+// allowedOrigins.js
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://www.dandrepairshop.com',
+    'https://dandrepairshop.com'
+]
+
+module.exports = allowedOrigins
