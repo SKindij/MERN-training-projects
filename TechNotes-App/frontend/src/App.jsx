496ba706825eb81d82aaeceea769a7a29d9e936b
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-10-24 17:41:00 +0300

    init: set up project structure and dependencies
    update App.jsx

--- TechNotes-App/frontend/src/App.jsx
+++ TechNotes-App/frontend/src/App.jsx
@@ -3,9 +3,10 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Public from './components/Public';
 import Login from './features/auth/Login';
-
 import DashLayout from './components/DashLayout';
 import Welcome from './features/auth/Welcome';
+import NotesList from './features/notes/NotesList';
+import UsersList from './features/users/UsersList';
 
 function App() {
   
@@ -14,14 +15,22 @@ function App() {
       <Route path="/" element={<Layout />} >
 	      {/* "привітальна сторінка" */}
         <Route index element={<Public />} />
-
+          {/* "" */}
         <Route path="login" element={<Login />} />		
 
         <Route path="dash" element={<DashLayout />}>
-		    {/* "DashHeader" */}
-          <Route index element={<Welcome />} />
-            {/* "DashFooter" */}
-		  
+		  {/* "DashHeader" */}
+            <Route index element={<Welcome />} />
+           
+			<Route path="notes">
+              <Route index element={<NotesList />} />
+            </Route>
+			
+			<Route path="users">
+              <Route index element={<UsersList />} />
+            </Route>
+			
+		  {/* "DashFooter" */}		  
         </Route>{/* End "dash" */}
 
       </Route>{/* End "/" */}
