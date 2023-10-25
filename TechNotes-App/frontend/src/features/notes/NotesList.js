b436c1fb5fa6e4729de948f3473e89983d222422
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-10-25 17:26:36 +0300

    feat: create  data visualization component
    update NotesList.js

--- TechNotes-App/frontend/src/features/notes/NotesList.js
+++ TechNotes-App/frontend/src/features/notes/NotesList.js
@@ -1,7 +1,50 @@
 // NotesList.jsx
+import { useGetNotesQuery } from "./notesApiSlice";
+import Note from "./Note";
+
 const NotesList = () => {
-    return (
-        <h2>NotesList</h2>
+	// здійснення запиту до API для отримання списку нотаток
+    const {
+        data: notes,
+        isLoading, isSuccess,
+        isError, error
+    } = useGetNotesQuery();
+
+    let content;
+
+    if (isLoading) content = <p>Loading...</p>;
+
+    if (isError) {
+        content = <p className="errmsg">{error?.data?.message}</p>
+    };
+  // після успішного запиту до API
+  if (isSuccess) {
+	// отримуєте ідентифікатори (ключі) нотаток з об'єкту notes
+    const { ids } = notes;
+    // створення вмісту для таблиці відображення нотаток
+    const tableContent = ids?.length
+      ? ids.map(noteId => <Note key={noteId} noteId={noteId} />)
+      : null
+
+    content = (
+      <table className="table table--notes">
+        <thead className="table__thead">
+          <tr>
+            <th scope="col" className="table__th note__status">Username</th>
+            <th scope="col" className="table__th note__created">Created</th>
+            <th scope="col" className="table__th note__updated">Updated</th>
+            <th scope="col" className="table__th note__title">Title</th>
+            <th scope="col" className="table__th note__username">Owner</th>
+            <th scope="col" className="table__th note__edit">Edit</th>
+          </tr>
+        </thead>
+        <tbody>
+            {tableContent}
+        </tbody>
+      </table>
     )
+  }
+
+    return content
 }
 export default NotesList
