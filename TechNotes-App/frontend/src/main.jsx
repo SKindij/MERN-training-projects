b75940cdf7515acb23377b554bb96f9c812c0abb
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-10-25 19:44:46 +0300

    feat: introduce new feature to codebase
    update main.jsx

--- TechNotes-App/frontend/src/main.jsx
+++ TechNotes-App/frontend/src/main.jsx
@@ -6,13 +6,18 @@ import App from './App.jsx'
 // для налаштування маршрутизації додатку
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 );
 
