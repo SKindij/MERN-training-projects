7fa6c7f7f55e6f0570e6065bf3bf199d254301fa
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-10-26 16:54:40 +0300

    feat: create new data visualization component
    update EditNote.jsx

--- TechNotes-App/frontend/src/features/notes/EditNote.jsx
+++ TechNotes-App/frontend/src/features/notes/EditNote.jsx
@@ -1,10 +1,22 @@
 // EditNote.jsx
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectNoteById } from './notesApiSlice';
+import { selectAllUsers } from '../users/usersApiSlice';
+import EditNoteForm from './EditNoteForm';
 
 const EditNote = () => {
-    
+	// отримуємо параметр "id" із URL
+    const { id } = useParams();
+    // вибираємо нотатку за її ID
+    const note = useSelector(state => selectNoteById(state, id));
+    // вибираємо всіх користувачів
+	const users = useSelector(selectAllUsers);
 
-    return <div>EditNote</div>
+  // перевіряємо, чи є нотатка та користувачі перед відображенням компоненту
+    const content = note && users ? <EditNoteForm note={note} users={users} /> : <p>Loading...</p>
+
+  // повертаємо вміст для відображення на сторінці
+    return content
 }
-export default EditNote
-
-
+export default EditNote;
