f432121e204c39c7ccccb28e12a948ec5b5c0c70
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-10-26 10:30:39 +0300

    feat: incorporate new user-facing feature
    update App.jsx

--- TechNotes-App/frontend/src/App.jsx
+++ TechNotes-App/frontend/src/App.jsx
@@ -8,6 +8,13 @@ import Welcome from './features/auth/Welcome';
 import NotesList from './features/notes/NotesList';
 import UsersList from './features/users/UsersList';
 
+import NewUserForm from './features/users/NewUserForm';
+import EditUser from './features/users/EditUser';
+
+import NewNote from './features/notes/NewNote';
+import EditNote from './features/notes/EditNote';
+
+
 function App() {
   
   return (
@@ -21,14 +28,20 @@ function App() {
         <Route path="dash" element={<DashLayout />}>
 		  {/* "DashHeader" */}
             <Route index element={<Welcome />} />
-           
-			<Route path="notes">
-              <Route index element={<NotesList />} />
-            </Route>
-			
+  
 			<Route path="users">
               <Route index element={<UsersList />} />
+			   <Route path=":id" element={<EditUser />} />
+               <Route path="new" element={<NewUserForm />} />
             </Route>
+
+			<Route path="notes">
+              <Route index element={<NotesList />} />
+			   <Route path=":id" element={<EditNote />} />
+               <Route path="new" element={<NewNote />} />
+            </Route>
+			
+
 			
 		  {/* "DashFooter" */}		  
         </Route>{/* End "dash" */}
