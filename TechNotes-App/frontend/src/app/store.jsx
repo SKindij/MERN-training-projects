57d201059544b87de9bb84dcd4ceff64448ee400
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-10-26 15:39:07 +0300

    perf: make changes to improve overall project performance
    update store.jsx

--- TechNotes-App/frontend/src/app/store.jsx
+++ TechNotes-App/frontend/src/app/store.jsx
@@ -1,6 +1,7 @@
 // store.jsx
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from './api/apiSlice';
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 // створення Redux-сховища з допомогою Redux Toolkit
 export const store = configureStore({
@@ -16,3 +17,18 @@ export const store = configureStore({
 	// активація інструментів розробника Redux DevTools у веб-браузері
     devTools: true
 })
+
+/*
+  програма може відправляти запити до сервера за допомогою Redux Toolkit Query
+  setupListeners встановлює підписку на їх відповіді та інші події
+  Коли результати запитів повертаються з сервера, setupListeners автоматично обробляє ці результати
+  та оновлює стан Redux-сховища відповідно до специфікації запитів
+*/
+setupListeners(store.dispatch);
+
+/*
+  Коли стан Redux-сховища оновлюється через обробку результатів запитів, 
+  це викликає перерендеринг компонентів, які використовують ці дані. 
+  Ось де реактивність приходить у гру. 
+  Компоненти, які залежать від даних з Redux-сховища, будуть автоматично оновлені при зміні даних.
+*/
