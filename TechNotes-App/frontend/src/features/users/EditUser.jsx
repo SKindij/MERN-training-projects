a2fd5adcabf717f1d555fbfdd0f6db9403193a12
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-10-26 12:35:23 +0300

    feat: incorporate new user-facing feature
    update EditUser.jsx

--- TechNotes-App/frontend/src/features/users/EditUser.jsx
+++ TechNotes-App/frontend/src/features/users/EditUser.jsx
@@ -1,9 +1,17 @@
 // EditUser.jsx
-
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectUserById } from './usersApiSlice';
+import EditUserForm from './EditUserForm';
 
 const EditUser = () => {
-    
+	// отримання параметру `id` з URL
+    const { id } = useParams()
+  // витягнення даних користувача за його ідентифікатором
+    const user = useSelector(state => selectUserById(state, id))
+  // визначення вмісту для відображення на сторінці
+    const content = user ? <EditUserForm user={user} /> : <p>Loading...</p>
 
-    return <div>EditUser</div>
+    return content
 }
-export default EditUser
+export default EditUser;
