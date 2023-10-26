0cb5a75b81a5a9b0271081592e2c7eb7d269ba0e
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-10-26 16:50:00 +0300

    feat: incorporate new user-facing feature
    update NewNoteForm.jsx

--- TechNotes-App/frontend/src/features/notes/NewNoteForm.jsx
+++ TechNotes-App/frontend/src/features/notes/NewNoteForm.jsx
@@ -1,10 +1,124 @@
 // NewNoteForm.jsx
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAddNewNoteMutation } from "./notesApiSlice";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSave } from "@fortawesome/free-solid-svg-icons";
 
-const NewNoteForm = () => {
-    
+const NewNoteForm = ({ users }) => {
+  // встановлюємо стан для нової нотатки та для управління її створенням
+    const [addNewNote, {
+        isLoading, isSuccess,
+        isError, error
+    }] = useAddNewNoteMutation();
 
-    return <div>NewNoteForm</div>
+    const navigate = useNavigate();
+
+    const [title, setTitle] = useState(''); // для назви нотатки
+    const [text, setText] = useState(''); // для тексту нотатки
+    const [userId, setUserId] = useState(users[0].id); // для вибору користувача
+
+  // очищаємо дані та переходимо на сторінку зі списком нотаток
+    useEffect(() => {
+        if (isSuccess) {
+            setTitle('')
+            setText('')
+            setUserId('')
+            navigate('/dash/notes')
+        }
+    }, [isSuccess, navigate]);
+
+  // обробники зміни назви та тексту нотатки
+    const onTitleChanged = e => setTitle(e.target.value);
+    const onTextChanged = e => setText(e.target.value);
+  // обробник зміни обраного користувача
+    const onUserIdChanged = e => setUserId(e.target.value);
+
+  // перевірка, чи можна зберегти нотатку
+    const canSave = [title, text, userId].every(Boolean) && !isLoading;
+
+  // обробник кнопки "Save", що відправляє нову нотатку на сервер
+    const onSaveNoteClicked = async (e) => {
+        e.preventDefault()
+        if (canSave) {
+            await addNewNote({ user: userId, title, text })
+        }
+    }
+
+    const options = users.map(user => {
+        return (
+            <option
+                key={user.id}
+                value={user.id}
+            > {user.username}</option >
+        )
+    })
+
+  // визначаємо клас для відображення помилки, якщо виникає
+    const errClass = isError ? "errmsg" : "offscreen"
+  // визначаємо класи для відображення незавершеної назви та тесту
+    const validTitleClass = !title ? "form__input--incomplete" : ''
+    const validTextClass = !text ? "form__input--incomplete" : ''
+
+    const content = (
+      <>
+        <p className={errClass}>{error?.data?.message}</p>
+
+        <form className="form" onSubmit={onSaveNoteClicked}>
+          <div className="form__title-row">
+            <h2>New Note</h2>
+            <div className="form__action-buttons">
+              <button
+                className="icon-button"
+                title="Save"
+                disabled={!canSave}
+               >
+                  <FontAwesomeIcon icon={faSave} />
+              </button>
+            </div>
+          </div>
+          <label className="form__label" htmlFor="title">
+              Title:
+		  </label>
+            <input
+              className={`form__input ${validTitleClass}`}
+              id="title"
+              name="title"
+              type="text"
+              autoComplete="off"
+              value={title}
+              onChange={onTitleChanged}
+            />
+
+          <label className="form__label" htmlFor="text">
+              Text:
+		  </label>
+            <textarea
+              className={`form__input form__input--text ${validTextClass}`}
+              id="text"
+              name="text"
+              value={text}
+              onChange={onTextChanged}
+            />
+
+          <label className="form__label form__checkbox-container" htmlFor="username">
+              ASSIGNED TO:
+		  </label>
+            <select
+              id="username"
+              name="username"
+              className="form__select"
+              value={userId}
+              onChange={onUserIdChanged}
+            >
+                {options}
+            </select>
+
+        </form>
+      </>
+    )
+
+    return content
 }
-export default NewNoteForm
 
-
+export default NewNoteForm;
